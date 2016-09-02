0b7c00befb651c7f733dfcb0f5a27c8ef710a3fd
Ruberto Paulo <rob.legend@gmail.com>
2016-09-02 11:52:04 +0200

    tested state on the topic

--- db/migrate/20160902093826_adding_state_to_topic.rb
+++ db/migrate/20160902093826_adding_state_to_topic.rb
@@ -1,5 +1,5 @@
 class AddingStateToTopic < ActiveRecord::Migration[5.0]
   def change
-    add_column :topics, :state, :integer
+    add_column :topics, :state, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902092706) do
+ActiveRecord::Schema.define(version: 20160902093826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20160902092706) do
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "author"
+    t.integer  "state",       default: 0
   end
 
 end
