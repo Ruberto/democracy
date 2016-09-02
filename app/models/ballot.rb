92204d0f825eab1cb060345fcfbe1aa881dbf8d6
Ruberto Paulo <rob.legend@gmail.com>
2016-09-02 15:25:52 +0200

    ballot changes

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -1,3 +1,26 @@
 class Ballot < ApplicationRecord
   validates_presence_of :name
+  validates_presence_of :ip_address
+  validates_uniqueness_of :ip_address
+
+  validates :ip_address, presence: true, uniqueness: true, format: { with: Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex) }
+
+  validate :topic_id_count
+  validate :topic_ids_are_open_topics
+
+  private
+
+  def topic_ids_are_open_topics
+    begin
+      self.topic_ids.map {|id| Topic.open.find(id) }
+    rescue ActiveRecord::RecordNotFound
+      errors.add(:topic_ids, 'Needs to be open topics')
+    end
+  end
+
+  def topic_id_count
+    if self.topic_ids.length != 3
+      errors.add(:topic_ids, 'Needs to be 3 topics only.')
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902093826) do
+ActiveRecord::Schema.define(version: 20160902123836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 20160902093826) do
   create_table "ballots", force: :cascade do |t|
     t.string   "ip_address"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.text     "topic_ids",  default: [],              array: true
   end
 
   create_table "topics", force: :cascade do |t|
