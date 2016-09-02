1f3286b975c0444a31ae9565493c9c1b38c33ae7
Ruberto Paulo <rob.legend@gmail.com>
2016-09-02 11:02:25 +0200

    added the topic model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902084928) do
+ActiveRecord::Schema.define(version: 20160902090057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+description 'User model' do
+  context 'it creates an user' do
+    xit 'creates a single user' do
+      # TODO: add stuff here
+    end
+  end
+end
