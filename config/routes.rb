8006688dcd81ad9c283d414f47af436f95d11479
Ashton <ashton@platform45.com>
2016-09-02 14:21:57 +0200

    cleanup unused assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :ballots
-  resources :topics
+  resources :topics, only: [:create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
